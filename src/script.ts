c542098444b6567ca1f0911633fbca19c7d314ff
Hilful <hilful25@gmail.com>
2022-02-19 22:03:51 +0600

    Type Alises v1.2

--- src/script.ts
+++ src/script.ts
@@ -54,6 +54,6 @@ const userDetails = (id: stringOrNum, user: userType) => {
   console.log(`User id is ${id}, name is ${user.name} and age is ${user.age}`);
 };
 
-const sayHello = (user: { name: string; age: number }) => {
+const sayHello = (user: userType) => {
   console.log(`Hello ${user.age > 50 ? "Sir" : "Mr."}`);
 };
