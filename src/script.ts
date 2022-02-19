74d0a76a1a1d35511d762f41c882d604ae432e53
Hilful <hilful25@gmail.com>
2022-02-20 01:09:28 +0600

    Tuples in typescript

--- src/script.ts
+++ src/script.ts
@@ -149,24 +149,29 @@
 // };
 
 /////ENUMS
-enum RType {
-  SUCCESS,
-  FAILURE,
-  UNAUTHENTICATED,
-  FORBIDDEN,
-}
-interface APIResponse<T> {
-  status: number;
-  type: RType;
-  data: T;
-}
+// enum RType {
+//   SUCCESS,
+//   FAILURE,
+//   UNAUTHENTICATED,
+//   FORBIDDEN,
+// }
+// interface APIResponse<T> {
+//   status: number;
+//   type: RType;
+//   data: T;
+// }
 
-const response1: APIResponse<object> = {
-  status: 200,
-  type: RType.FORBIDDEN,
-  data: {
-    name: "Test",
-    something: 300,
-  },
-};
-console.log(response1);
+// const response1: APIResponse<object> = {
+//   status: 200,
+//   type: RType.FORBIDDEN,
+//   data: {
+//     name: "Test",
+//     something: 300,
+//   },
+// };
+// console.log(response1);
+
+//Tuples
+
+let a = [3, "Hello", { p: 3 }];
+let b: [number, string, object] = [4, "world", { q: 2 }];
