1f8e794da6aee677bf7a320a1d3c5b5e04964998
Hilful <hilful25@gmail.com>
2022-02-19 22:28:07 +0600

    function signature final

--- src/script.ts
+++ src/script.ts
@@ -61,12 +61,25 @@
 
 //Function Signature Typescript
 
-let calculation: (x: number, y: number, z: string) => number;
-calculation = (a: number, b: number, c: string) => {
-  if (c === "add") {
-    return a + b;
-  } else {
-    return a - b;
+let userDetails: (
+  x: number | string,
+  userInfo: {
+    name: string;
+    age: number;
   }
-};
-console.log(calculation(5, 6, "minus"));
+) => void;
+// calculation = (a: number, b: number, c: string) => {
+//   if (c === "add") {
+//     return a + b;
+//   } else {
+//     return a - b;
+//   }
+// };
+// console.log(calculation(5, 6, "minus"));
+userDetails = (
+  id: number | string,
+  user: {
+    name: string;
+    age: number;
+  }
+) => {};
