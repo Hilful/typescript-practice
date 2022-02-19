fb548cba5efd35c69d0a46fb15dba5065299f19c
Hilful <hilful25@gmail.com>
2022-02-19 21:19:29 +0600

    dynamic type or any type

--- src/script.ts
+++ src/script.ts
@@ -6,7 +6,29 @@
 
 // playerName = 36;
 // console.log(playerName);
-function multiply(a: number, b: number) {
-  return a * b;
-}
-console.log(multiply(3, 4));
+// function multiply(a: number, b: number) {
+//   return a * b;
+// }
+// console.log(multiply(3, 4));
+
+// let a: string;
+// let b: number;
+// //for arrays
+// let a : string[] = [];
+// a.push("this is a value")
+// let a : (string | number) [] = [];
+
+let a: any[] = [];
+a.push("Bangladesh");
+a.push(34);
+console.log(a);
+
+//object
+let b: {
+  name: any;
+  age: any;
+};
+b = {
+  name: "Bangladesh",
+  age: 45,
+};
