fbece58dacd9fef3bcdb8447c02291f3233270c7
Hilful <hilful25@gmail.com>
2022-02-19 23:00:36 +0600

    classes in typescript v1

--- src/script.ts
+++ src/script.ts
@@ -61,25 +61,49 @@
 
 //Function Signature Typescript
 
-let userDetails: (
-  x: number | string,
-  userInfo: {
-    name: string;
-    age: number;
-  }
-) => void;
-// calculation = (a: number, b: number, c: string) => {
-//   if (c === "add") {
-//     return a + b;
-//   } else {
-//     return a - b;
+// let userDetails: (
+//   x: number | string,
+//   userInfo: {
+//     name: string;
+//     age: number;
 //   }
-// };
-// console.log(calculation(5, 6, "minus"));
-userDetails = (
-  id: number | string,
-  user: {
-    name: string;
-    age: number;
+// ) => void;
+// // calculation = (a: number, b: number, c: string) => {
+// //   if (c === "add") {
+// //     return a + b;
+// //   } else {
+// //     return a - b;
+// //   }
+// // };
+// // console.log(calculation(5, 6, "minus"));
+// userDetails = (
+//   id: number | string,
+//   user: {
+//     name: string;
+//     age: number;
+//   }
+// ) => {};
+
+//Classes in Typescript
+
+class Player {
+  name: string;
+  age: number;
+  country: string;
+
+  constructor(n: string, a: number, c: string) {
+    this.name = n;
+    this.age = a;
+    this.country = c;
   }
-) => {};
+  play() {
+    console.log(`${this.name} from ${this.country} is playing`);
+  }
+}
+
+const mashrafi = new Player("Mashrafi", 40, "Bangladesh");
+const sakib = new Player("Sakib", 40, "Bangladesh");
+
+const players: Player[] = [];
+players.push(sakib);
+players.push(mashrafi);
