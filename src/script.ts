ae215317763c2b7bf17025c1ec3552257c238670
Hilful <hilful25@gmail.com>
2022-02-19 22:03:15 +0600

    Type Alises v1.1

--- src/script.ts
+++ src/script.ts
@@ -45,14 +45,12 @@
 // myFunc("1", "2");
 
 type stringOrNum = string | number;
+type userType = {
+  name: string;
+  age: number;
+};
 
-const userDetails = (
-  id: stringOrNum,
-  user: {
-    name: string;
-    age: number;
-  }
-) => {
+const userDetails = (id: stringOrNum, user: userType) => {
   console.log(`User id is ${id}, name is ${user.name} and age is ${user.age}`);
 };
 
